0f30b271092e80d1d4eb1c51c19971970375edb5
eskil4152 <eskiltb@hotmail.com>
2024-02-29 19:43:57 +0100

    fix memory leak with integer file

--- src/createFile.c
+++ src/createFile.c
@@ -8,7 +8,7 @@ void createFile(){
     FILE* file;
 
     printf("Please provide a name for the file you want to write to \n");
-    char fileName[24];
+    char fileName[256];
 
     scanf("%s", fileName);
     sprintf(fileName, "%s.txt", fileName);
@@ -29,5 +29,6 @@ void createFile(){
         break;
     }
     
+    free(fileContentType);
     fclose(file);
 }
